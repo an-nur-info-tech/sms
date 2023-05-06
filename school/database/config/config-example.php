e2f9b5fd3f1a01cc5da1da6c19c9f23a82188174
an-nur-info-tech <wasiubello60@gmail.com>
2023-05-06 12:26:32 +0100

    ISSSUE-0: Add database file and configuration manual in the readme

--- school/database/config/config-example.php
+++ school/database/config/config-example.php
@@ -1,7 +1,12 @@
 <?php
-  // Rename config-example to config 
+  /*
+  
+  Rename config-example to config 
+   
+  */
+
   // DB Params
   define("DB_HOST", "localhost");
   define("DB_USER", "root");
   define("DB_PASS", "");
-  define("DB_NAME", ""); // Input database name inside the double quotes
+  define("DB_NAME", "sms"); 
